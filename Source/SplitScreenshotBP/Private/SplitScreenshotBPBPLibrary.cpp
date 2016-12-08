0bf27a83ea9b74377bd27108ff9ad6ca6038ac82
Ricardo Maes <ricmzn@gmail.com>
2016-12-08 19:02:15 -0200

    Initial commit

---
+++ Source/SplitScreenshotBP/Private/SplitScreenshotBPBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "SplitScreenshotBPPrivatePCH.h"
+#include "SplitScreenshotBPBPLibrary.h"
+
+USplitScreenshotBPBPLibrary::USplitScreenshotBPBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USplitScreenshotBPBPLibrary::SplitScreenshotBPSampleFunction(float Param)
+{
+	return -1;
+}
+
